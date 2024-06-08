8693b316377f06ecd6763437c8f16d80acf436d7
Cleber RD <roberto.doria7@gmail.com>
2024-06-08 02:36:49 -0300

    commit único

---
+++ src/containers/ListaVagas/index.tsx
@@ -0,0 +1,120 @@
+import { useState } from 'react'
+import FormVagas from '../../components/FormVagas'
+
+import Vaga from '../../components/Vaga'
+
+import { S } from './styles'
+
+type Vaga = {
+  id: string
+  titulo: string
+  localizacao: string
+  nivel: string
+  modalidade: string
+  salarioMin: number
+  salarioMax: number
+  requisitos: string[]
+}
+
+const vagas = [
+  {
+    id: 1,
+    titulo: 'Desenvolvedor front-end',
+    localizacao: 'remoto',
+    nivel: 'junior',
+    modalidade: 'clt',
+    salarioMin: 3000,
+    salarioMax: 4500,
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+  },
+  {
+    id: 2,
+    titulo: 'Dentista de Lobisomem',
+    localizacao: 'São Paulo/SP',
+    nivel: 'especialista_veteranisomem',
+    modalidade: 'clt',
+    salarioMin: 45000,
+    salarioMax: 65000,
+    requisitos: ['MUITO_FORTE', 'MANEJAR_ARMAS', '5_ANOS_EXPERIÊNCIA', 'FORMAÇÃO_VETERINÁRIA']
+  },
+  {
+    id: 3,
+    titulo: 'Desenvolvedor fullstack',
+    localizacao: 'remoto',
+    nivel: 'pleno',
+    modalidade: 'pj',
+    salarioMin: 4000,
+    salarioMax: 6000,
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+  },
+  {
+    id: 4,
+    titulo: 'Designer de portais dimensionais',
+    localizacao: 'São Paulo/SP',
+    nivel: 'pleno',
+    modalidade: 'clt',
+    salarioMin: 400000,
+    salarioMax: 500000,
+    requisitos: ['Física_Teorica', 'Tecnologia_Inter-estelar', 'conhecimentos_de_5ª_matéria', 'QI_acima_de_200']
+  },
+  {
+    id: 5,
+    titulo: 'Desenvolvedor front-end',
+    localizacao: 'remoto',
+    nivel: 'senior',
+    modalidade: 'clt',
+    salarioMin: 7000,
+    salarioMax: 8000,
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+  },
+  {
+    id: 6,
+    titulo: 'Desenvolvedor front-end para projeto internacional',
+    localizacao: 'remoto',
+    nivel: 'senior',
+    modalidade: 'pj',
+    salarioMin: 12000,
+    salarioMax: 15000,
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+  },
+  {
+    id: 7,
+    titulo: 'Desenvolvedor front-end',
+    localizacao: 'São Paulo/SP',
+    nivel: 'junior',
+    modalidade: 'clt',
+    salarioMin: 4000,
+    salarioMax: 5000,
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+  }
+]
+
+const ListaVagas = () => {
+  const [filtro, setFiltro] = useState<string>('')
+
+  const vagasFiltradas = vagas.filter(
+    (x) => x.titulo.toLocaleLowerCase().search(filtro) >= 0
+  )
+
+  return (
+    <div>
+      <FormVagas aoPesquisar={(termo: string) => setFiltro(termo)} />
+      <S.JobOpening>
+        {vagasFiltradas.map((vag) => (
+          <Vaga
+            key={vag.id}
+            titulo={vag.titulo}
+            localizacao={vag.localizacao}
+            nivel={vag.nivel}
+            modalidade={vag.modalidade}
+            salarioMin={vag.salarioMin}
+            salarioMax={vag.salarioMax}
+            requisitos={vag.requisitos}
+          />
+        ))}
+      </S.JobOpening>
+    </div>
+  )
+}
+
+export default ListaVagas
